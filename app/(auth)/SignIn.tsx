1572c51365f5e55569e93dee44701b4b9f6a4f2b
AceTheDeveloper <ghostwrexker@gmail.com>
2026-08-08 14:43:48 +0800

    Added login screen

---
+++ app/(auth)/SignIn.tsx
@@ -0,0 +1,108 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import { Text } from "@/components/ui/text";
+import React from "react";
+import { Image, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function SignIn() {
+  return (
+    <SafeAreaView className="flex-1 bg-brand-bg">
+      <View className="p-6 flex-1 justify-center">
+        {/* Header Section */}
+        <View className="flex-row items-center gap-3 mb-10">
+          <Text className="text-brand-secondary font-header-bold text-3xl">
+            Welcome Back
+          </Text>
+          <Image
+            source={require("../../assets/icons/hand.png")}
+            className="size-8"
+            resizeMode="contain"
+          />
+        </View>
+
+        {/* Form Section */}
+        <View className="gap-6">
+          <View className="gap-2">
+            <Label
+              className="font-body-medium text-brand-muted"
+              nativeID="email-label"
+            >
+              Email
+            </Label>
+            <Input
+              placeholder="Enter your email"
+              className="font-body focus:border-brand-primary"
+              aria-labelledby="email-label"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+          </View>
+
+          <View className="gap-2">
+            <Label
+              className="font-body-medium text-brand-muted"
+              nativeID="password-label"
+            >
+              Password
+            </Label>
+            <Input
+              placeholder="Enter your password"
+              secureTextEntry
+              className="font-body focus:border-brand-primary"
+              aria-labelledby="password-label"
+            />
+            <TouchableOpacity className="self-end mt-1">
+              <Text className="text-brand-primary font-body-semibold text-sm">
+                Forgot Password?
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          <Button
+            className="bg-brand-primary mt-4 py-3 rounded-xl shadow-sm p-2"
+            size={"lg"}
+          >
+            <Text className="text-white font-header-bold text-lg">Login</Text>
+          </Button>
+        </View>
+
+        {/* Divider Section */}
+        <View className="flex-row items-center justify-between my-8 gap-4">
+          <Separator className="flex-1 bg-brand-muted/30" />
+          <Text className="text-brand-muted font-body-medium text-sm">
+            Or log in with
+          </Text>
+          <Separator className="flex-1 bg-brand-muted/30" />
+        </View>
+
+        <View>
+          <Button
+            className="border border-neutral-300 bg-white py-3 rounded-xl shadow-sm p-2"
+            size={"lg"}
+          >
+            <View className="flex-row items-center gap-2">
+              <Image
+                source={require("../../assets/icons/google.png")}
+                className="size-6"
+              />
+              <Text className="text-black font-body-medium">Google</Text>
+            </View>
+          </Button>
+        </View>
+
+        {/* Footer Section */}
+        <View className="flex-row justify-center items-center mt-auto mb-4 gap-1">
+          <Text className="text-brand-muted font-body">
+            Don&apos;t have an account?
+          </Text>
+          <TouchableOpacity>
+            <Text className="text-brand-secondary font-body-bold">Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,59 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import "@/global.css";
+import { NAV_THEME } from "@/lib/theme";
+import { ThemeProvider } from "@react-navigation/native";
+import { PortalHost } from "@rn-primitives/portal";
+import { Stack, router } from "expo-router";
+import * as SplashScreen from "expo-splash-screen"; // 1. Import SplashScreen
+import { useEffect } from "react";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
+import {
+  Nunito_400Regular,
+  Nunito_500Medium,
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+} from "@expo-google-fonts/nunito";
+import { useFonts } from "expo-font";
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = "light";
+
+  const [fontsLoaded, fontError] = useFonts({
+    Nunito_400Regular,
+    Nunito_500Medium,
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+
+      router.replace("/(auth)/SignIn");
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+    <ThemeProvider value={NAV_THEME[colorScheme]}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
       </Stack>
-      <StatusBar style="auto" />
+      <PortalHost />
     </ThemeProvider>
   );
 }
